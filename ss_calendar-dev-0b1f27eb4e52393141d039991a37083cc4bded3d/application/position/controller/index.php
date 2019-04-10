004d29d6266e058cb33bb13def009af3c4c93400
juliezhang1112 <1060074157@qq.com>
2019-04-10 11:01:26 +0800

    加入高高代码

--- ss_calendar-dev-0b1f27eb4e52393141d039991a37083cc4bded3d/application/position/controller/index.php
+++ ss_calendar-dev-0b1f27eb4e52393141d039991a37083cc4bded3d/application/position/controller/index.php
@@ -15,5 +15,31 @@ class index
         // 返回视图
 
 
+
+        //列表
+        public function user_position_list() {
+            $list = Db::table('user_position')
+                ->select();
+            $this->assign("list", $list);
+            return $this->fetch();
+        }
+
+        //作废
+        function invalid($user_id) {
+            $data = array();
+            $data['is_delete'] = 1;
+            $data['delete_time'] =  Db::raw('now()');
+            Db::table('user_position')->where('id', $user_id)->update($data);
+            $this->redirect('/index/index/user_position_list');
+        }
+        //恢复
+        function restore($user_id) {
+            $data = array();
+            $data['is_delete'] = 0;
+            $data['delete_time'] =  Db::raw('now()');
+            Db::table('user_position')->where('id', $user_id)->update($data);
+            $this->redirect('/index/index/user_position_list');
+        }
+    }
     }
 }
